a746f79fdb9c420c6ac0723d7714ff6a3f3f0f18
tony940125 <tonytsai0125@gmail.com>
2025-06-27 11:51:12 +0800

    練習一

---
+++ ds_01.java
@@ -0,0 +1,18 @@
+// 檔名：ds_01.java
+import java.util.Scanner;
+
+public class ds_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("請輸入第一個整數：");
+        int num1 = scanner.nextInt();
+
+        System.out.print("請輸入第二個整數：");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("兩數之和為：" + sum);
+    }
+}
